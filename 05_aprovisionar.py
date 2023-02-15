5b2558d91d7a6633df59609ce4082bbb0d4dd468
EC2 Default User <ec2-user@ip-172-31-66-16.ec2.internal>
2023-02-15 08:38:51 +0000

    script de aprovisionamiento modificado y pipeline incluye su ejecucion

--- 05_aprovisionar.py
+++ 05_aprovisionar.py
@@ -1,36 +1,45 @@
 import boto3
 import os
+import time
 
 
 #cambiar las llaves de acceso y region
-os.environ['AWS_ACCESS_KEY_ID']= 'XXXXX'
-os.environ['AWS_SECRET_ACCESS_KEY']= 'XXXXX'
-os.environ ['AWS_DEFAULT_REGION']=  'region'
 
+print(os.environ['AWS_ACCESS_KEY_ID'])
+print(os.environ['AWS_SECRET_ACCESS_KEY'])
+print(os.environ ['AWS_DEFAULT_REGION'])
+print(os.environ['ECR_REPO_NAME'])
+print(os.environ['ACCOUNT_NUMBER'])
 
 
 #crear repositorio de imagen de contenedores - Elastic Container Registry
+
+print("Creando Registro de Imagen del contenedor")
 ecr_cliente = client = boto3.client('ecr')
 
-client.create_repository(repositoryName='dummylinear')
+client.create_repository(repositoryName=os.environ['ECR_REPO_NAME'])
+
+#esperar 30 segundos
 
 #crear cluster de Elastic Container Service
 
 ecs_cliente = boto3.client('ecs')
 
 #crear defincion de Tarea
+print("Creando Definicion de Tarea de ECS")
+
 ecs_cliente.register_task_definition( 
-     family='tskdummylinear2',
+     family='task{}'.format(os.environ['ECR_REPO_NAME']),
      cpu ='256',
      memory = '512',
      requiresCompatibilities= ['FARGATE'],
      runtimePlatform = {'cpuArchitecture': 'X86_64', 'operatingSystemFamily': 'LINUX'},
      networkMode = 'awsvpc' ,
-     executionRoleArn = 'arn:aws:iam::740327929864:role/ecsTaskExecutionRole',
+     executionRoleArn = 'arn:aws:iam::{}:role/ecsTaskExecutionRole'.format(os.environ['ACCOUNT_NUMBER']),
      containerDefinitions=[
          { 
             'name': 'dummylinear',
-            'image': '740327929864.dkr.ecr.us-east-1.amazonaws.com/dummylinear',
+            'image': '{}.dkr.ecr.{}.amazonaws.com/{}'.format(os.environ['ACCOUNT_NUMBER'],os.environ ['AWS_DEFAULT_REGION'],os.environ['ECR_REPO_NAME']) ),
             'portMappings': [{'containerPort': 5000, 'hostPort': 5000, 'protocol': 'tcp', 'name': 'dummylinear-5000-tcp',
                               'appProtocol': 'http'}] ,
          }
@@ -38,10 +47,14 @@ ecs_cliente.register_task_definition(
     
  )
  
+#esperar 30 segundos
+time.sleep(30)
 #Crear servicio
+print("Creando Servicio de ECS")
+
 ecs_cliente.create_service(cluster='dummylinear',
                            serviceName='svcdummylinear2',
-                           taskDefinition='arn:aws:ecs:us-east-1:740327929864:task-definition/tskdummylinear:1',
+                           taskDefinition='arn:aws:ecs:{}:{}:task-definition/{}:1'.format(os.environ ['AWS_DEFAULT_REGION'],os.environ['ACCOUNT_NUMBER'],,os.environ['ECR_REPO_NAME']),
                            launchType ='FARGATE',
                            desiredCount=1,
                            networkConfiguration= {
@@ -60,6 +73,6 @@ ecs_cliente.create_service(cluster='dummylinear',
                           
 
 #mostrar definciones de tare y servicios en JSON
-ecs_cliente.describe_task_definition( taskDefinition='tskdummylinear2') 
+#ecs_cliente.describe_task_definition( taskDefinition='tskdummylinear2') 
 
-ecs_cliente.describe_services(cluster='dummylinear',services=["svcdummylinear"])
+#ecs_cliente.describe_services(cluster='dummylinear',services=["svcdummylinear"])
